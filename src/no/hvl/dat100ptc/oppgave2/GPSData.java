aa1307f1c5b121ff49f2e3c4a63215b93e17d0d4
LillyFro <184953@stud.hvl.no>
2024-10-16 20:21:24 +0200

    Oppgave 2 Ferdig

--- src/no/hvl/dat100ptc/oppgave2/GPSData.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSData.java
@@ -3,6 +3,8 @@ package no.hvl.dat100ptc.oppgave2;
 import no.hvl.dat100ptc.TODO;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
+import java.lang.reflect.Array;
+
 public class GPSData {
 
 	private GPSPoint[] gpspoints;
@@ -19,30 +21,30 @@ public class GPSData {
 	}
 	
 	protected boolean insertGPS(GPSPoint gpspoint) {
-
 		boolean inserted = false;
-		for (int i=0; i < gpspoints.length; i++  ) {
-			gpspoints[antall] = gpspoint;
-			antall++;
+		if (antall >= gpspoints.length) {
+			return inserted;
 		}
+			gpspoints[antall] = gpspoint;
+			
 		inserted = true;
+		antall++;
 		return inserted;
 	}
 
 	public boolean insert(String time, String latitude, String longitude, String elevation) {
-
-		GPSPoint gpspoint;
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		GPSPoint gpspoint = GPSDataConverter.convert(time, latitude, longitude, elevation);
+		boolean insert = insertGPS(gpspoint);
+		return insert;
 		
 	}
 
 	public void print() {
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO 
+		System.out.println("====== GPS Data - START ======");
+		for (int i = 0; i < gpspoints.length; i++) {
+			System.out.println(gpspoints[i]);
+		}
+		System.out.println("====== GPS Data - SLUTT ======");
+		
 	}
 }
